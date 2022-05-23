923cc927d15693db6d35a7d806393827363a496f
Sumeet Patel <sumeet.patel@eroam.com>
2022-05-23 09:32:40 +0530

    added component files.

---
+++ src/views/Home.vue
@@ -0,0 +1,121 @@
+<template>
+  <div class="home">    
+    <!-- <Header></Header> -->
+    <b-navbar toggleable="sm" type="light" variant="light" style="border-bottom:1px solid #efefef;">
+      <div class="container">
+        <b-navbar-brand href="#">NavBar</b-navbar-brand>
+        <b-form-checkbox switch size="sm" v-model="themeMode">Dark Mode</b-form-checkbox>
+      </div>
+    </b-navbar>  
+    <div class="container py-5">
+      <b-button v-b-modal.modal-1 variant="primary" class="mb-3">New Use</b-button>
+      <b-list-group>
+        <template v-if="users.length">
+          <b-list-group-item
+            v-for="user, i in users" :key="i">
+            <div class="row d-flex align-items-center">
+              <div class="col-auto">
+                <b-form-checkbox
+                  id="checkbox-1"
+                  v-model="user.checked"
+                  name="checkbox-1"
+                  @change="changeChecked(i)"
+                >
+                </b-form-checkbox>
+              </div>
+              <div class="col-auto">
+                <b-avatar></b-avatar>
+              </div>
+              <div class="col">
+                <span :style="user.checked?'text-decoration:line-through':null" :class="{'font-italic':user.checked}">
+                {{user.name}}
+                </span>
+              </div>
+            </div>                        
+          </b-list-group-item>
+        </template>
+        <template v-else>
+          <b-list-group-item>No Users Found !!</b-list-group-item>
+        </template>
+      </b-list-group>
+    </div>
+
+    <b-modal centered size="lg" id="modal-1" @ok="saveUser()" title="Add New User">
+      <b-form-group
+        id="input-group-1"
+        label="Email address:"
+        label-for="input-1"
+        description="We'll never share your email with anyone else."
+      >
+        <b-form-input
+          id="input-1"
+          v-model="user.email"
+          type="email"
+          placeholder="Enter email"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-2" label="Your Name:" label-for="input-2">
+        <b-form-input
+          id="input-2"
+          v-model="user.name"
+          placeholder="Enter name"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-3" label="Favorite Food:" label-for="input-3">
+        <b-form-select
+          id="input-3"
+          v-model="user.food"
+          :options="foods"
+          class="form-control"
+        ></b-form-select>
+      </b-form-group>
+
+      <b-form-group id="input-group-4">
+        <b-form-checkbox v-model="user.checked">Active</b-form-checkbox>
+      </b-form-group>
+    </b-modal>
+
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+@Component({
+  components: {
+  },
+})
+export default class Home extends Vue {
+  users:any=[];
+  themeMode:any=false;
+  user:any={
+    email:'',
+    name:'',
+    food:null,
+    checked:false,
+  }
+
+  foods:any= [{ text: 'Select One', value: null }, 'Carrots', 'Beans', 'Tomatoes', 'Corn']
+
+  mounted(){
+    this.users = this.$store.state.user;
+  }
+
+  saveUser(){
+    this.$store.dispatch('addNewUser',this.user);
+    this.users = this.$store.state.user;
+  }
+
+  changeChecked(index:any){
+    this.$store.dispatch('changeStatus',index);
+  }
+
+}
+</script>
+
+<style scoped>
+  .form-group{
+    margin-bottom: 20px;
+  }
+</style>
