eb9aed46e076b26ab569f54f402f0db2bccb979d
Sumeet Patel <sumeet.patel@eroam.com>
2022-06-22 18:06:37 +0530

    updated next button and breadscrumb

--- src/components/addressInformation.vue
+++ src/components/addressInformation.vue
@@ -7,15 +7,13 @@
       <h5>{{ componentValue.Name }}</h5>
     </div>
 
-    <form>
+    <form @submit.prevent.stop="saveAddress">
       <div class="row">
         <div class="col-md-12">
           <div class="form-group">
             <label for="">Address Line 1</label>
             <b-form-input
               id="input-1"
-              v-model="firstName"
-              type="email"
               placeholder="Enter Address Line 1"
             ></b-form-input>
           </div>
@@ -25,8 +23,6 @@
             <label for="">Address Line 2</label>
             <b-form-input
               id="input-1"
-              v-model="firstName"
-              type="email"
               placeholder="Enter Address Line 2"
             ></b-form-input>
           </div>
@@ -36,8 +32,6 @@
             <label for="">City</label>
             <b-form-input
               id="input-1"
-              v-model="firstName"
-              type="email"
               placeholder="Enter City"
             ></b-form-input>
           </div>
@@ -47,8 +41,6 @@
             <label for="">State</label>
             <b-form-input
               id="input-1"
-              v-model="firstName"
-              type="email"
               placeholder="Enter State"
             ></b-form-input>
           </div>
@@ -58,15 +50,13 @@
             <label for="">Contry</label>
             <b-form-input
               id="input-1"
-              v-model="firstName"
-              type="email"
               placeholder="Enter Country"
             ></b-form-input>
           </div>
         </div>
       </div>
         <div class="mt-5">
-            <b-button variant="success">continue to card details</b-button>
+            <b-button type="submit" variant="success">continue to card details</b-button>
             <b-link class="text-light-gray ml-5">skip for now</b-link>
         </div>
     </form>
@@ -79,6 +69,10 @@ import { Component, Vue, Prop } from "vue-property-decorator";
 @Component
 export default class addressInformation extends Vue {
     @Prop() componentValue:any;
+
+    saveAddress(){
+      this.$emit('incrementComponent', this.componentValue)
+    }
 }
 </script>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,12 @@
 <template>
-  <div>   
+  <div>
     <b-container>
       <b-breadcrumb>
-        <b-breadcrumb-item v-for="item, i in userDetailComponents" :key="i" :active="isSelectedComponent[0].Name == item.Name">{{item.Name}}</b-breadcrumb-item>
+        <b-breadcrumb-item v-for="(item, i) in breadscrumbComponent" :key="i" :active="isCurrentComponent.Name == item.Name">
+          {{ item.Name }}
+        </b-breadcrumb-item>
       </b-breadcrumb>
     </b-container>
-
     <b-container>
       <div class="form-div">
         <div class="row">
@@ -13,8 +14,9 @@
             <keep-alive>
               <component
                 @incrementComponent="nextComponent"
-                :is="isSelectedComponent[0].component"
-                :componentValue="isSelectedComponent[0]">
+                :is="isCurrentComponent.componentName"
+                :componentValue="isCurrentComponent"
+              >
               </component>
             </keep-alive>
           </div>
@@ -22,19 +24,29 @@
             <div class="card card-light-gray mb-5">
               <div class="card-body">
                 <h5 class="mb-3">Hi there, welcome here</h5>
-                <p class="mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+                <p class="mb-0">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco.
+                </p>
               </div>
             </div>
 
             <div class="card overflow-hidden">
-              <img :src="userImage" alt="" srcset="">
+              <img :src="userImage" alt="" srcset="" />
               <div class="card-body">
-                <p class="mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
-                <b-button class="mt-5" variant="light-info" block>Upload a Photo</b-button>
+                <p class="mb-0">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco.
+                </p>
+                <b-button class="mt-5" variant="light-info" block
+                  >Upload a Photo</b-button
+                >
               </div>
             </div>
           </div>
-        </div>      
+        </div>
       </div>
     </b-container>
 
@@ -89,35 +101,70 @@ import addressInformation from "@/components/addressInformation.vue";
 import cardInformation from "@/components/cardInformation.vue";
 import uploadImage from "@/components/uploadImage.vue";
 @Component({
-  components:{
+  components: {
     PersonaIInformation,
     addressInformation,
     cardInformation,
-    uploadImage
-  }
+    uploadImage,
+  },
 })
-export default class Home extends Vue {    
-  profileIcon:any=require("@/assets/images/profile.svg");
-  addressIcon:any=require("@/assets/images/address.svg");
-  cardIcon:any=require("@/assets/images/card.svg");
-  imageIcon:any=require("@/assets/images/profileImg.svg");
-  userImage:any=require("@/assets/images/userimage.jpg");
+export default class Home extends Vue {
+  profileIcon: any = require("@/assets/images/profile.svg");
+  addressIcon: any = require("@/assets/images/address.svg");
+  cardIcon: any = require("@/assets/images/card.svg");
+  imageIcon: any = require("@/assets/images/profileImg.svg");
+  userImage: any = require("@/assets/images/userimage.jpg");
 
+  isCurrentComponent: any = {};
+  breadscrumbComponent:any=[];
 
-  userDetailComponents:any=[
-    {id:1,Name:'Personal Information', component:'PersonaIInformation', icon:this.profileIcon, isActive:true, isCompleted:true},
-    {id:2,Name:'Address Information', component:'addressInformation', icon:this.addressIcon, isActive:false, isCompleted:false},
-    {id:3,Name:'Card Details', component:'cardInformation', icon:this.cardIcon, isActive:false, isCompleted:false},
-    {id:4,Name:'Upload Profile Image', component:'uploadImage', icon:this.imageIcon, isActive:false, isCompleted:false},
-  ]
+  userDetailComponents: any = [
+    {
+      id: 1,
+      Name: "Personal Information",
+      componentName: "PersonaIInformation",
+      icon: this.profileIcon,
+      isActive: true,
+      isCompleted: true,
+    },
+    {
+      id: 2,
+      Name: "Address Information",
+      componentName: "addressInformation",
+      icon: this.addressIcon,
+      isActive: false,
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      Name: "Card Details",
+      componentName: "cardInformation",
+      icon: this.cardIcon,
+      isActive: false,
+      isCompleted: false,
+    },
+    {
+      id: 4,
+      Name: "Upload Profile Image",
+      componentName: "uploadImage",
+      icon: this.imageIcon,
+      isActive: false,
+      isCompleted: false,
+    },
+  ];
 
-  get isSelectedComponent(){
-    let component = this.userDetailComponents.filter((f) => f.isActive == true);    
-    return component;
+  mounted() {
+    this.isCurrentComponent = this.userDetailComponents[0]; 
+    this.breadscrumbComponent = this.userDetailComponents.filter((b) => b.isCompleted == true); 
   }
 
-  nextComponent(value){
-    value.isActive = false;
+  nextComponent(value) {
+    let index = value.id - 1;
+    Vue.set(this.userDetailComponents[index], "isActive", false);
+    Vue.set(this.userDetailComponents[index + 1], "isActive", true);
+    Vue.set(this.userDetailComponents[index + 1], "isCompleted", true);
+    this.isCurrentComponent = this.userDetailComponents[index + 1];
+    this.breadscrumbComponent = this.userDetailComponents.filter((b) => b.isCompleted == true);
   }
 }
 </script>
